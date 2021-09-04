adbad745737b9921b881f682743752966f6d198c
Piyush Bag <piyushbag4@gmail.com>
2021-09-04 14:10:26 -0700

    Created first Model and Serializer

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.9
 #setting PYTHONBUFFERED to a nonempty value means
 # that the python output is sent to the terminal without 
-#bieng buffered and we can see the output in real time
+#being buffered and we can see the output in real time
 # (eg.django logs) 
 ENV PYTHONUNBUFFERED 1
 WORKDIR /app
--- admin/settings.py
+++ admin/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'corssheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'admin.urls'
